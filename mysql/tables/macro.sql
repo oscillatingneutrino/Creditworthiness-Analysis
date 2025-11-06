20e08b5c031b22d7bf9660ac45fed7c7ebe63935
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-05 17:20:05 -0800

    Create macro.sql

---
+++ mysql/tables/macro.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `macro` (
+  `region` text,
+  `industry` text,
+  `gdp_growth` double DEFAULT NULL,
+  `inflation_rate` double DEFAULT NULL,
+  `avg_interest_rate` double DEFAULT NULL
+)
