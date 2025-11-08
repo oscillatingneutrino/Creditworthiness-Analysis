c3ef9ea8caee242436875a5bca9314a5b01517fd
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-08 12:46:44 -0800

    Update linear_regression.py

--- python_cleanup/linear_regression.py
+++ python_cleanup/linear_regression.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 
-df = pd.read_csv('/Users/d404me/Downloads/final_results_rated_creditworthiness - final_results_rated.csv.csv')
+df = pd.read_csv('final_results.csv')
 
-predictors = df[["tlr", "sdlr", "ebit_coverage_ratio", "ebidta_coverage_ratio", "capex_adjusted_ratio",
+predictors = df[["tlr", "sdlr", "ebit_coverage_ratio", "ebitda_coverage_ratio", "capex_adjusted_coverage_ratio",
               "AR", "AP","inventory_days", "CCC", "debt_to_equity", "operating_cash_flow_coverage",
               "profit_margin", "liquidity_ratio"]]
 
